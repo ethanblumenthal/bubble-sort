ec947d6e0296fbf87948489f5002d3bf9d30145d
Ethan Blumenthal <ethan.blumenthal@gmail.com>
2019-01-14 11:04:01 -0600

    completed bubble sort

--- bubblesort.js
+++ bubblesort.js
@@ -1,3 +1,16 @@
-function bubbleSort(array) {
-  
+function bubbleSort(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length - i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        swap(arr, j, j + 1);
+      }
+    }
+  }
+  return arr;
+}
+
+function swap(arr, idx1, idx2) {
+  const temp = arr[idx1];
+  arr[idx1] = arr[idx2];
+  arr[idx2] = temp;
 }
--- bubblesort.spec.js
+++ bubblesort.spec.js
@@ -8,6 +8,17 @@ describe('Bubble Sort', function(){
   });
 
   it('handles an array with multiple elements', function(){
-    expect( bubbleSort([3, 1, 2]) ).toEqual( [1, 2, 3] );
+    expect( bubbleSort([5, 1, 4, 3, 2]) ).toEqual( [1, 2, 3, 4, 5] );
+  });
+});
+
+describe('Swap', function(){
+  beforeAll(function () {
+    spyOn(window, 'swap').and.callThrough();
+  });
+
+  it('checks the number of swaps to sort', function () {
+    bubbleSort([5, 1, 4, 3, 2]);
+    expect(window.swap.calls.count()).toEqual(7);
   });
 });
